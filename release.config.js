2b4d6901003413177c7b6a8467f0eb32167368b7
MUHAMMED FAYAZ T S <muhammedfayazts01@gmail.com>
2026-05-15 12:57:16 +0530

    refactor(ci): simplify semantic-release workflow

--- release.config.js
+++ release.config.js
@@ -1,30 +1,18 @@
 module.exports = {
   branches: ["main"],
-  repositoryUrl: "https://github.com/MuhammedFayazTS/easystart-cli.git",
+
+  repositoryUrl:
+    "https://github.com/MuhammedFayazTS/easystart-cli.git",
 
   plugins: [
     "@semantic-release/commit-analyzer",
+
     "@semantic-release/release-notes-generator",
 
     [
-      "@semantic-release/changelog",
+      "@semantic-release/npm",
       {
-        changelogFile: "CHANGELOG.md",
-      },
-    ],
-
-    "@semantic-release/npm",
-
-    [
-      "@semantic-release/git",
-      {
-        assets: [
-          "CHANGELOG.md",
-          "package.json",
-          "package-lock.json",
-        ],
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+        npmPublish: true,
       },
     ],
 
